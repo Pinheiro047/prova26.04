af17ab35ae89098c66df847557fd787a68b4616c
Felipe <Pinheirofelipe741@gmail.com>
2023-04-26 12:08:10 -0300

    html pronto

--- valor.js
+++ valor.js
@@ -2,9 +2,6 @@ let calcular = document.getElementById('calcular')
 let resultado = document.getElementById('resultado')
 let preco_final = document.getElementById('preco_final')
 
-let qntd_prod = []
-let preco_prod = []
-
 calcular.addEventListener('click' , ()=>{
     let produto = document.getElementById('produto').value
     let preco_prod = Number(document.getElementById('preco_prod').value)
